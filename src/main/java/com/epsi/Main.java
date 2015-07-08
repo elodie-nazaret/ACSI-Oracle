eaa3815158528ea6dc6bd7f07c914ef6160fa45a
axel <a>
2015-07-08 21:08:45 +0200

    développement de la page d'accueil

--- src/main/java/com/epsi/Main.java
+++ src/main/java/com/epsi/Main.java
@@ -1,5 +1,9 @@
 package com.epsi;
 
+import com.epsi.entities.ArticleDAO;
+import com.epsi.forms.ArticleForm;
+import com.epsi.forms.CreateUpdateArticleForm;
+import com.epsi.forms.HomeForm;
 import com.epsi.forms.SubscribeForm;
 
 import java.lang.String;
@@ -7,6 +11,10 @@ import java.lang.String;
 public class Main {
 
     public static void main(String[] args) {
-        new SubscribeForm();
+        ArticleDAO articleDAO = new ArticleDAO();
+//        new CreateUpdateArticleForm();
+//        new CreateUpdateArticleForm(articleDAO.getArticleById(21));
+//        new ArticleForm(articleDAO.getArticleById(21));
+        new HomeForm();
     }
 }
--- src/main/java/com/epsi/forms/HomeForm.java
+++ src/main/java/com/epsi/forms/HomeForm.java
@@ -1,10 +1,67 @@
 package com.epsi.forms;
 
-import javax.swing.*;
+import com.epsi.entities.Admin;
+import com.epsi.entities.Article;
+import com.epsi.entities.ArticleDAO;
+import com.epsi.managers.Connection;
 
-/**
- * Created by Elodie on 07/07/2015.
- */
-public class HomeForm {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class HomeForm extends JFrame implements ActionListener {
     private JPanel root;
+    private JPanel gridPanel;
+    private JPanel bottomPanel;
+    private JLabel newSubscribersText;
+    private JLabel totalSubscribersText;
+    private JButton addArticleButton;
+    private JButton statisticsButton;
+    private JPanel statPanel;
+    private JPanel buttonPanel;
+
+
+    public HomeForm() {
+        setContentPane(root);
+        setResizable(false);
+
+        GridLayout grid = new GridLayout(-1, 3, 5, 5);
+
+        this.gridPanel.setLayout(grid);
+
+        ArticleDAO articleDAO = new ArticleDAO();
+
+        for (Article article : articleDAO.getAllArticles()) {
+            JPanel jPanel = this.setArticleForm(article);
+            this.gridPanel.add(jPanel);
+        }
+
+        this.statisticsButton.addActionListener(this);
+        this.addArticleButton.addActionListener(this);
+        pack();
+
+        setVisible(true);
+    }
+
+
+    private JPanel setArticleForm(Article article) {
+        HomeArticle homeArticle = new HomeArticle(article);
+        return homeArticle.getAll();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.statisticsButton) {
+            if (Connection.getInstance().getConnectedPeople() instanceof Admin) {
+                new StatsAdminForm();
+            }
+            else {
+                new StatsVisitorForm();
+            }
+        }
+        else if (e.getSource() == this.addArticleButton) {
+            new CreateUpdateArticleForm();
+        }
+    }
 }
