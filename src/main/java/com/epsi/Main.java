36d3e4e4d531935a1a0a6afc1aba841b81ed27b6
axel <a>
2015-07-09 08:42:31 +0200

    hash des password

--- src/main/java/com/epsi/Main.java
+++ src/main/java/com/epsi/Main.java
@@ -1,16 +1,10 @@
 package com.epsi;
 
-import com.epsi.entities.ArticleDAO;
 import com.epsi.forms.ConnectionForm;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArticleDAO articleDAO = new ArticleDAO();
-//        new CreateUpdateArticleForm();
-//        new CreateUpdateArticleForm(articleDAO.getArticleById(21));
-//        new ArticleForm(articleDAO.getArticleById(21));
-//        new HomeForm();
         new ConnectionForm();
     }
 }
